e9dd3650c247492ab78b1f09e2ef5e522a50b697
motypes <mo@openfren.com>
2023-05-12 17:48:26 +0800

    initialising auth

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,9 +1,12 @@
+import NewChat from "./NewChat"
+
 const Sidebar = () => {
     return (
         <div className="p-2 flex flex-col h-screen">
             <div className="flex-1">
                 <div>
                     {/* New Chat */}
+                    <NewChat />
                     <div>
                         {/* Model Selection */}
                     </div>
