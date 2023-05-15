e6fec823bfe4d0cef07a195f12fda93f3f5bb830
motypes <mo@openfren.com>
2023-05-15 16:43:37 +0800

    implemented signout as well as singin

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,22 +1,35 @@
-import NewChat from "./NewChat"
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
+import NewChat from "./NewChat";
 
 const Sidebar = () => {
-    return (
-        <div className="p-2 flex flex-col h-screen">
-            <div className="flex-1">
-                <div>
-                    {/* New Chat */}
-                    <NewChat />
-                    <div>
-                        {/* Model Selection */}
-                    </div>
+  const { data: session } = useSession();
+  return (
+    <div className="p-2 flex flex-col h-screen">
+      <div className="flex-1">
+        <div>
+          {/* New Chat */}
+          <NewChat />
+          <div>{/* Model Selection */}</div>
 
-                    {/* Map throgh he Chart rows here */}
-                </div>
-
-            </div>
+          {/* Map throgh he Chart rows here */}
         </div>
-    )
-}
+      </div>
+      {session && (
+        // on hover change cursor to pointer
+        
+        <div className="flex flex-col text-slate-200/50 text-center hover:cursor-pointer hover:bg-slate-200/10 
+        p-2 rounded-md"
+        onClick={() => signOut()}
+        >
+          {/* User Info */}
+          Logged In: {session.user?.email}
+          {/* Logout */}
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
